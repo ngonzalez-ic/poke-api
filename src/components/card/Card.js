78ba4f1875f20859e9dc3c61d27b3eb05fdf327c
ngonzalez-ic <cgonzalez@iiconcepcion.edu.ar>
2021-07-15 14:01:56 -0300

    language

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,10 +1,12 @@
 import React,{useState} from 'react'
 import BgImg from '../../images/bg-pattern-card.svg'
 import Ball from '../../images/buttom/pokeball.png'
+import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 
 const Card =(props)=>{
   const { pokemon }=props
+  const {t }= useTranslation()
   const [ footer, setfooter] = useState(true)
   const handleOnClick = ()=>{
     if(footer===true){
@@ -49,15 +51,15 @@ const Card =(props)=>{
         
             )
           })}
-        <p>Type</p>
+        <p>{t('Type')}</p>
       </div>
       <div className="card-footer-social">
         <h3>803K</h3>
-        <p>Ataque Especial</p>
+        <p>{t('EspecialAtack')}</p>
       </div>
       <div className="card-footer-social">
         <h3>1.4K</h3>
-        <p>Defensa</p>
+        <p>{t('Def')}</p>
       </div>
     </div>
 }
---
+++ src/config/i18next.js
@@ -0,0 +1,30 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next';
+
+const userLang = navigator.language || navigator.userLanguage;
+const defaultLocale = userLang.substring(0,2)
+i18n
+.use(initReactI18next)
+.init({
+    resources:{
+        en:{
+            translation:{
+                Type:'Type',
+                EspecialAtack:'Attack Especial',
+                Def:'Defense'    
+            }
+        },
+        es:{
+            translation:{
+                Type:'Tipo',
+                EspecialAtack:'Ataque especial',
+                Def:'Defensa'
+            }
+        }
+    },
+    lng: defaultLocale,
+    fallbackLng: 'es',
+    interpolation:{
+        escapeValue:false
+    }
+})
